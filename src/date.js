5693a7b10fab565bf37210e75306dd8f86a97102
Gabrielle von Koss <gabrielle@gvonkoss.com>
2023-10-08 15:50:57 +0200

    some cleanup

--- src/date.js
+++ src/date.js
@@ -1,7 +1,7 @@
 export const buildDate = () => {
   const today = new Date();
-  const locale = navigator.language;
-
+  // const locale = navigator.language; // not mvp!
+  const locale = 'en-GB';
   const time = document.querySelector('time');
   time.setAttribute('datetime', today.toISOString());
 
